f982a3103c65c2cd18c2ac0704d14fad5968271e
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-23 21:20:26 +0530

    [Aniket]- Validating First Name

--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace RegexPattern
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegexPattern
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("***Welcome To Regular Expression***");
+            //UC1
+            Validation person = new Validation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstName = Console.ReadLine();
+            person.ValidateFirstName(firstName);
+
         }
     }
 }
